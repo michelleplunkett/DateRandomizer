8a574f17b3bec7b5b079094550838c9c65c279f5
michelleplunkett <michellep1994@gmail.com>
2023-02-01 22:05:56 -0600

    add feature to search yelp API by your location

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import pandas as pd
+import yelpapi
+import os 
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -25,18 +28,48 @@ def generate():
     budget = data.get("budget")
     alcohol = data.get("alcohol")
     location = data.get("location")
+    geo = data.get("geo")
     preparation = data.get("preparation")
 
     # Define the logic here 
-    def generate_activity(formality, budget, alcohol, location, preparation):
+    def generate_activity(formality, budget, alcohol, location, geo, preparation):
         df = pd.read_csv('static/item_list.csv')
 
+        # Budget map dict 
         budget_map = {
             'Free': 0,
             '$': 1,
             '$$': 2,
             '$$$': 3
         }
+        
+        # Logic to call the Yelp API 
+        if (budget != 'Free') and (location != 'Home') and (geo != ''):
+            print(os.getenv('YELP_API_KEY'))
+            yelp_api = yelpapi.YelpAPI(os.getenv('YELP_API_KEY'))
+
+            def get_restaurants(latitude, longitude, budget):
+                response = yelp_api.search_query(term="restaurants", latitude=latitude, longitude=longitude, radius=16093, price=budget_map[budget]+1, open_now=True, limit=10) # 16093.4 meters in 10 miles
+                restaurants = response["businesses"]
+                return restaurants
+
+            lat = float(geo.split(',')[0].strip())
+            long = float(geo.split(',')[1].strip())
+
+            res = get_restaurants(lat, long, '$')
+            
+            # select random result
+            final = res[random.randint(0, len(res) - 1)]
+
+            # function to get HTML from response
+            def parse_restaurant(res):
+                name = res['name']
+                url = res['url']
+                return '<a href="' + url + '">' + name + '</a>'
+
+            food = parse_restaurant(final)
+        else: 
+            food = df[df["Category"] == "Food"].sample(1)["Item"].values[0]
         
         # For activity, food, treat, dress (unique category) 
         # filter the df and return a random value 
@@ -56,7 +89,6 @@ def generate():
 
         # Randomly select a row
         activity = df[df["Category"] == "Activity"].sample(1)["Item"].values[0]
-        food = df[df["Category"] == "Food"].sample(1)["Item"].values[0]
         treat = df[df["Category"] == "Treat"].sample(1)["Item"].values[0]
         dress = df[df["Category"] == "Dress"].sample(1)["Item"].values[0]        
         
@@ -68,7 +100,7 @@ def generate():
         }
 
     # Use the form data to generate the date night suggestions
-    res = generate_activity(formality, budget, alcohol, location, preparation)
+    res = generate_activity(formality, budget, alcohol, location, geo, preparation)
 
     return jsonify({
         "activity": res['activity'],
--- static/app.js
+++ static/app.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   var generateButton = document.getElementById("generate-button");
   var dateNightResult = document.getElementById("date-night-result");
   var bookmarkButton = document.getElementById("bookmark-button");
+  var geoButton = document.getElementById("geo-button");
 
+  // Button logic to call flask API to generate results 
   generateButton.addEventListener("click", function (event) {
     event.preventDefault();
 
@@ -24,15 +26,19 @@ document.addEventListener("DOMContentLoaded", function () {
         dateNightResult.style.display = "block";
       }
     };
+
     xhr.send(JSON.stringify({
       formality: document.getElementById("formality-preference").value,
       budget: document.getElementById("budget-preference").value,
       alcohol: document.getElementById("alcohol-preference").checked,
       location: document.getElementById("location-preference").value,
+      geo: document.getElementById("geo-preference").value,
       preparation: document.getElementById("preparation-preference").checked
     }));
+
   });
 
+  // Bookmark button logic 
   bookmarkButton.addEventListener("click", function (event) {
     event.preventDefault();
 
@@ -47,7 +53,52 @@ document.addEventListener("DOMContentLoaded", function () {
     let result = document.getElementById("result").innerHTML;
 
     saveResult(result);
-    
+
+  });
+
+  // Geo locator for the location button
+  geoButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        document.getElementById("geo-preference").value = "Retrieving location...";
+        var timeoutVal = 10 * 1000;
+        navigator.geolocation.getCurrentPosition(
+          showPosition,
+          showError, {
+            enableHighAccuracy: true,
+            timeout: timeoutVal,
+            maximumAge: 0
+          }
+        );
+      } else {
+        document.getElementById("geo-preference").value = "Geolocation is not supported by this browser.";
+      }
+    }
+
+    function showPosition(position) {
+      document.getElementById("geo-preference").value = position.coords.latitude + ", " + position.coords.longitude;
+    }
+
+    function showError(error) {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          document.getElementById("geo-preference").value = "User denied the request for Geolocation.";
+          break;
+        case error.POSITION_UNAVAILABLE:
+          document.getElementById("geo-preference").value = "Location information is unavailable.";
+          break;
+        case error.TIMEOUT:
+          document.getElementById("geo-preference").value = "The request to get user location timed out.";
+        case error.UNKNOWN_ERROR:
+          document.getElementById("geo-preference").value = "An unknown error occurred.";
+          break;
+      }
+    }
+
+    getLocation();
+
   });
 
 });
